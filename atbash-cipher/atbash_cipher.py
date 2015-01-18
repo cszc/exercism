98ab7021bb6e402d7bcc0c0787d19aaf159c831c
Christine Chung <cszchung@gmail.com>
2015-01-17 19:46:08 -0500

    completed atbash

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -1,13 +1,18 @@
-TABLE = str(" "*96 + "zyxwvutsrqponmlkjihgfedcba" + " "*5)
+import string
+
+TABLE = str(" "*48 + "0123456789" + " "*39 + "zyxwvutsrqponmlkjihgfedcba" + " "*133)
 
 def decode(text):
-  text = text.lower.replace(' ', '')
-  message = string.translate(text, TABLE, string.punctuation)
-  return message
+    text = text.lower().replace(' ', '')
+    message = string.translate(text, TABLE, string.punctuation)
+    return message
 
 def encode(text):
-  text = text.lowercase.replace(' ', '')
-  message = string.translate(text, TABLE, string.punctuation)
-  return message
-
-import string
+    text = text.lower().replace(' ', '')
+    message = string.translate(text, TABLE, string.punctuation)
+    formattedMessage = ""
+    for i in range(len(message)):
+        if i % 5 == 0 and i != 0:
+            formattedMessage += " "
+        formattedMessage += message[i]
+    return formattedMessage
